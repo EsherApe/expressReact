650a5de51425a9cda2f5dbb0a35c5942ccbc36d0
funkybo <esher5580@gmail.com>
2017-09-20 15:22:55 +0300

    redux-thunk, redux-devtools-extension

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import {createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 
 //====================================
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 //============================================================
 
 // components
