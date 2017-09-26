4e210120bbe9104863e1d4ea8263068c90fd1a58
funkybo <esher5580@gmail.com>
2017-09-26 18:03:03 +0300

    server session

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require('../models/user').User;
 
 router.post('/save', (req, res) => {
-    if(!req.body) return req.sendStatus(400);
+    if (!req.body) return req.sendStatus(400);
 
     const user = new User({
         login: req.body.login,
@@ -17,7 +17,7 @@ router.post('/save', (req, res) => {
 
     user.save((err, user) => {
         if (err) {
-            if(err.code == 11000) {
+            if (err.code == 11000) {
                 res.setHeader('Content-Type', 'text/html');
                 res.send({text: 'This user is already exist!', error: true});
             }
@@ -40,20 +40,23 @@ router.post('/login', (req, res, next) => {
     let password = req.body.password,
         email = req.body.email;
 
+    function sendError() {
+        res.send({error: 403, message: 'Wrong E-mail or password'});
+        next(createError(403, 'Wrong E-mail or password'));
+    }
+
     User.findOne({email: email}, (err, user) => {
         if (err) return next(err);
         return user;
     }).then((user) => {
         if (user) {
-            if(user.checkPasswords(password)) {
+            if (user.checkPasswords(password)) {
                 return user;
             } else {
-                res.send({error: true, message: 'Wrong E-mail or password'});
-                next(createError(403, 'Wrong E-mail or password'));
+                sendError();
             }
         } else {
-            res.send({error: true, message: 'Wrong E-mail or password'});
-            next(createError(403, 'Wrong E-mail or password'));
+            sendError();
         }
     }).then(user => {
         let loggedUser = {
@@ -65,8 +68,11 @@ router.post('/login', (req, res, next) => {
             birthday: user.birthday,
             isLogin: true
         };
+        req.session.user = loggedUser.id;
         res.send(loggedUser);
-    }).catch(err => {console.log(err);})
+    }).catch(err => {
+        console.log(err);
+    })
 });
 
 module.exports = router;
