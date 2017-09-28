a030e92a35d3440784adb431a29033bd79b4944e
esherape <esher5580@gmail.com>
2017-09-28 23:44:43 +0300

    temporary comment

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const User = require('../models/user').User;
 
 router.get('/', (req, res, next) => {
-    if(req.session.user) {
-        User.findById(req.session.user, (err, user) => {
-            if(err) return next(err);
-            return user;
-        }).then(user => {
-            let respUser = {
-                id: user._id,
-                login: user.login,
-                lastName: user.lastName,
-                firstName: user.firstName,
-                email: user.email,
-                birthday: user.birthday,
-                isLogin: true
-            };
-            console.log(respUser);
-            res.send(respUser);
-        });
-    }
+    // res.setHeader('Content-Type', 'application/json');
+    // if(req.session.user) {
+    //     User.findById(req.session.user, (err, user) => {
+    //         if(err) return next(err);
+    //         return user;
+    //     }).then(user => {
+    //         let respUser = {
+    //             id: user._id,
+    //             login: user.login,
+    //             lastName: user.lastName,
+    //             firstName: user.firstName,
+    //             email: user.email,
+    //             birthday: user.birthday,
+    //             isLogin: true
+    //         };
+    //         res.send(respUser);
+    //     }).catch(err => console.error(err));
+    // }
     res.render('index');
 });
 
